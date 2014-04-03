8be0515ddcdd79a8567bce08c744ec282456e5a9
Atom <Atom@machinerule.com>
2014-04-03 16:25:47 -0500

    Create _package.lisp

---
+++ qbo_talk/srv_gen/lisp/_package.lisp
@@ -0,0 +1,9 @@
+(cl:defpackage qbo_talk-srv
+  (:use )
+  (:export
+   "TEXT2SPEACH"
+   "<TEXT2SPEACH-REQUEST>"
+   "TEXT2SPEACH-REQUEST"
+   "<TEXT2SPEACH-RESPONSE>"
+   "TEXT2SPEACH-RESPONSE"
+  ))
